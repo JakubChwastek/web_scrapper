e5fecddff5e981ad2c4679805f1a92f47aebded4
Jakub Chwastek <kuba.chwastek95@gmail.com>
2020-02-12 18:36:15 +0100

    db connection

--- flatfinder.py
+++ flatfinder.py
@@ -1,27 +1,34 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import pandas as pd
+import psycopg2
+import sqlalchemy as db
+from sqlalchemy.ext.declarative import declarative_base
+
+base = declarative_base()
+
+engine = db.create_engine('postgresql://flats_db.sql')
+engine.connect()
+
 
 driver  = webdriver.Chrome("\webdrver\chromedriver.exe")
 
-coins = []
+flats = []
 prices = []
 market_cap = []
-driver.get("https://coinmarketcap.com/")
+driver.get("https://www.olx.pl/nieruchomosci/mieszkania/sprzedaz/gdansk/?search%5Bdistrict_id%5D=99")
 
 content = driver.page_source
 soup = BeautifulSoup(content, 'html.parser')
-myString = "&nbsp;"
 
-for a in soup.find_all('tr', attrs = {'class':'cmc-table-row'}):
-    name = a.find('div', attrs = {'class':'cmc-table__column-name sc-1kxikfi-0 eTVhdN'}).find('a', attrs = {'class':'cmc-link'})
-    price = a.find('td', attrs = {'class':'cmc-table__cell cmc-table__cell--sortable cmc-table__cell--right cmc-table__cell--sort-by__price'}).find('a', attrs = {'class':'cmc-link'})
-    volume = a.find('td', attrs = {'class':'cmc-table__cell cmc-table__cell--sortable cmc-table__cell--right cmc-table__cell--sort-by__market-cap'}).find('div', attrs = {'class':''})
-    prices.append(price.text)
-    coins.append(name.text)
-    vol = volume.text
-    vol1 = vol.replace(r'\xa',' ')
-    market_cap.append(vol1)
-money = zip(prices, market_cap)
-out_table = dict(zip(coins,money))
+for a in soup.find('table', attrs={'id':'offers_table'}).find_all('td', attrs = {'class':'offer'}):
+    name = a.find('a', href=True,  attrs = {'class':'marginright5 link linkWithHash detailsLink'})
+    price = a.find('p', attrs = {'class':'price'})
+    if name is not None:
+        unified_name =  name.text.replace('\n', '')
+        flats.append(unified_name)
+    if price is not None:
+        unified_price = price.text.replace('\n', '')
+        prices.append(unified_price)
+
+out_table = dict(zip(flats,prices))
 print(out_table)
---
+++ flats_db.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE "Flats_db"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Polish_Poland.1250'
+    LC_CTYPE = 'Polish_Poland.1250'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
